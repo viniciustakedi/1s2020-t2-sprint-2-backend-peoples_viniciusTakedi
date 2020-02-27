4ce21fd9191054e1e3695bc1e6c92eb434a9c73a
Tak3edi <viniciustakedi7@gmail.com>
2020-02-27 13:52:53 -0300

    projeto peoples (27/02)

---
+++ Back-end/Senai.People.WebApi/Senai.People.WebApi/Controllers/FuncionarioController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Peoples.webAPI.Interfaces;
+using Senai.Peoples.webAPI.Repositories;
+using Senai.Peoples.webAPI.Domains;
+
+namespace Senai.Peoples.webAPI.Controllers
+{
+       [Produces("application/json")]
+
+       [Route("api/[controller]")]
+
+       [ApiController]
+    public class FuncionarioController : ControllerBase
+    {
+        private IFuncionarioRepository _funcionarioRepository { get; set; }
+
+        public FuncionarioController()
+        {
+            _funcionarioRepository = new FuncionarioRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<FuncionarioDomain> Get()
+        {
+            return _funcionarioRepository.Listar();
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(FuncionarioDomain novoFuncionario)
+        {
+            _funcionarioRepository.Inserir(novoFuncionario);
+
+            return StatusCode(201);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            FuncionarioDomain funcionarioBuscado = _funcionarioRepository.BuscarPorId(id);
+
+            if (funcionarioBuscado == null)
+            {
+                return NotFound("Não foi encontrado nunhum funcionario");
+            }
+            return Ok(funcionarioBuscado);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutIdUrl(int id, FuncionarioDomain funcionarioAtualizado)
+        {
+           FuncionarioDomain funcionarioBuscado = _funcionarioRepository.BuscarPorId(id);
+
+            if (funcionarioBuscado == null)
+            {
+                return NotFound
+                    (
+                        new
+                        {
+                            mensagem = "Funcionario não pode ser encontrado",
+                        }
+                    );
+            }
+
+            try
+            {
+               _funcionarioRepository.AtualizarPorUrl(id, funcionarioAtualizado);
+
+                return NoContent();
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _funcionarioRepository.Deletar(id);
+            return Ok("Funcionario foi deletado");
+        }
+    }
+}
